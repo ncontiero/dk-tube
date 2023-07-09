104d550f19ad7d3abd08b654f1af1c6fdbe3b444
DKSHS <88355080+ShadowsS01@users.noreply.github.com>
2023-07-09 19:58:48 -0300

    feat: removing animation in tabs

--- src/components/ChannelTabs/ChannelTabs.tsx
+++ src/components/ChannelTabs/ChannelTabs.tsx
@@ -9,7 +9,6 @@ import { userFormatter } from "@/utils/formatters";
 import Link from "next/link";
 import * as Tabs from "@radix-ui/react-tabs";
 import { ChannelVideoCard } from "./components";
-import { AnimatePresence, motion, type MotionProps } from "framer-motion";
 
 import { ChevronDown } from "lucide-react";
 
@@ -17,18 +16,10 @@ interface ChannelTabsProps {
   user: UserWithVideosAndPlaylists;
 }
 
-type TabsContentProps = MotionProps & Tabs.TabsContentProps;
-
-function TabsContent({ className, ...props }: TabsContentProps) {
-  const MotionTabsContent = motion(Tabs.Content);
-
+function TabsContent({ className, ...props }: Tabs.TabsContentProps) {
   return (
-    <MotionTabsContent
+    <Tabs.Content
       className={`w-full outline-none ring-purple-500 focus-visible:ring-2 ${className}`}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.2 }}
       {...props}
     />
   );
@@ -87,69 +78,59 @@ export function ChannelTabs({ user }: ChannelTabsProps) {
         </div>
       </Tabs.List>
       <div className="mx-auto flex w-full max-w-screen-2xl xs:px-2">
-        <AnimatePresence>
-          {currentTab === "home" ? (
-            <TabsContent value="home" forceMount>
-              <div className="w-full pb-6 xs:max-w-5xl xs:pt-3">
-                <ChannelVideoCard
-                  video={userVideos[0]}
-                  variant={screenWidth > 590 ? "main" : "large"}
-                />
-              </div>
-              <div className="overflow-hidden border-t border-zinc-700 px-3 pt-6">
-                <Link
-                  className="font-semibold outline-none ring-purple-500 duration-200 focus-visible:ring-2"
-                  href={`/channel/${user.id}?tab=videos`}
-                >
-                  Vídeos
-                </Link>
-                <div className="relative mt-3 flex w-full snap-x snap-mandatory flex-col gap-2 overflow-x-auto pb-6 xs:max-w-5xl xs:flex-row xs:gap-3 xs:pt-3">
-                  {userVideos
-                    .filter((video) => video.id !== userVideos[0].id)
-                    .slice(0, screenWidth < 590 ? amountOfVideos : 12)
-                    .map((video) => (
-                      <ChannelVideoCard
-                        key={video.id}
-                        video={video}
-                        variant="small"
-                      />
-                    ))}
-                  {screenWidth < 590 && userVideos.length > amountOfVideos && (
-                    <button
-                      type="button"
-                      className="flex items-center self-center rounded-full bg-zinc-900 p-2 duration-200 hover:bg-zinc-700 focus:bg-zinc-500 focus:outline-none"
-                      onClick={() => setAmountOfVideos((prev) => prev + 3)}
-                    >
-                      <ChevronDown />
-                    </button>
-                  )}
-                </div>
-              </div>
-            </TabsContent>
-          ) : currentTab === "videos" ? (
-            <TabsContent value="home" forceMount>
-              <div className="flex w-full grid-cols-2 flex-col gap-7 pb-6 xs:grid xs:gap-4 xs:pl-1 xs:pt-3 mdlg:grid-cols-4">
-                {userVideos.map((video) => (
+        <TabsContent value="home">
+          <div className="w-full pb-6 xs:max-w-5xl xs:pt-3">
+            <ChannelVideoCard
+              video={userVideos[0]}
+              variant={screenWidth > 590 ? "main" : "large"}
+            />
+          </div>
+          <div className="overflow-hidden border-t border-zinc-700 px-3 pt-6">
+            <Link
+              className="font-semibold outline-none ring-purple-500 duration-200 focus-visible:ring-2"
+              href={`/channel/${user.id}?tab=videos`}
+            >
+              Vídeos
+            </Link>
+            <div className="relative mt-3 flex w-full snap-x snap-mandatory flex-col gap-2 overflow-x-auto pb-6 xs:flex-row xs:gap-3 xs:pt-3">
+              {userVideos
+                .filter((video) => video.id !== userVideos[0].id)
+                .slice(0, screenWidth < 590 ? amountOfVideos : 12)
+                .map((video) => (
                   <ChannelVideoCard
                     key={video.id}
                     video={video}
-                    variant="large"
+                    variant="small"
                   />
                 ))}
+              {screenWidth < 590 && userVideos.length > amountOfVideos && (
+                <button
+                  type="button"
+                  className="flex items-center self-center rounded-full bg-zinc-900 p-2 duration-200 hover:bg-zinc-700 focus:bg-zinc-500 focus:outline-none"
+                  onClick={() => setAmountOfVideos((prev) => prev + 3)}
+                >
+                  <ChevronDown />
+                </button>
+              )}
+            </div>
+          </div>
+        </TabsContent>
+        <TabsContent value="videos">
+          <div className="flex w-full grid-cols-2 flex-col gap-7 pb-6 xs:grid xs:gap-4 xs:pl-1 xs:pt-3 mdlg:grid-cols-4">
+            {userVideos.map((video) => (
+              <ChannelVideoCard key={video.id} video={video} variant="large" />
+            ))}
+          </div>
+        </TabsContent>
+        <TabsContent value="playlists">
+          <div className="flex w-full grid-cols-2 flex-col gap-7 pb-6 xs:grid xs:gap-4 xs:pl-1 xs:pt-3 mdlg:grid-cols-4">
+            {user.playlists.map((playlist) => (
+              <div key={playlist.id} className="h-full">
+                <h3>{playlist.name}</h3>
               </div>
-            </TabsContent>
-          ) : (
-            <TabsContent value="playlists" forceMount>
-              <div className="flex w-full grid-cols-2 flex-col gap-7 pb-6 xs:grid xs:gap-4 xs:pl-1 xs:pt-3 mdlg:grid-cols-4">
-                {user.playlists.map((playlist) => (
-                  <div key={playlist.id} className="h-full">
-                    <h3>{playlist.name}</h3>
-                  </div>
-                ))}
-              </div>
-            </TabsContent>
-          )}
-        </AnimatePresence>
+            ))}
+          </div>
+        </TabsContent>
       </div>
     </Tabs.Root>
   );
