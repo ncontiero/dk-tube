03928336ee048b990e16f8a084d6ed86f4b738af
DKSHS <88355080+ShadowsS01@users.noreply.github.com>
2023-07-14 19:08:58 -0300

    feat: adding search page to public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api(.*)", "/channel(.*)", "/watch"],
+  publicRoutes: ["/", "/api(.*)", "/channel(.*)", "/watch", "/search"],
 });
 
 export const config = {
