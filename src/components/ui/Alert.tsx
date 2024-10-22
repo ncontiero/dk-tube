bc7646078676c2ec7682eb49226ced450af24bd9
dkshs <88355080+dkshs@users.noreply.github.com>
2024-10-22 17:49:50 -0300

    chore(deps): update @dkshs/eslint-config to 3.3.0

--- src/components/ui/Alert.tsx
+++ src/components/ui/Alert.tsx
@@ -35,12 +35,14 @@ Alert.displayName = "Alert";
 const AlertTitle = forwardRef<
   HTMLParagraphElement,
   HTMLAttributes<HTMLHeadingElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <h5
     ref={ref}
     className={cn("mb-1 font-medium leading-none tracking-tight", className)}
     {...props}
-  />
+  >
+    {children}
+  </h5>
 ));
 AlertTitle.displayName = "AlertTitle";
 
