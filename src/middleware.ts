f6c6991fbd37c7e53c65bb7402139451d9aab9af
dkshs <88355080+dkshs@users.noreply.github.com>
2024-11-05 19:03:11 -0300

    chore: add playlist pages in clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/create-video(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/create-video(.*)",
+  "/playlist/WL",
+  "/playlist/LL",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
