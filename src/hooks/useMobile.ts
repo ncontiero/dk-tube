a35ca6bcd12dacee6aab3635f9128976e2d4690f
DKSHS <88355080+dkshs@users.noreply.github.com>
2024-11-12 19:50:07 -0300

    fix: update isMobile hook

--- src/hooks/useMobile.ts
+++ src/hooks/useMobile.ts
@@ -8,12 +8,12 @@ export function useIsMobile(isMobileC?: boolean) {
   useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
     const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT ?? isMobileC);
     };
     mql.addEventListener("change", onChange);
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT ?? isMobileC);
     return () => mql.removeEventListener("change", onChange);
   }, []);
 
-  return isMobileC ?? !!isMobile;
+  return !!isMobile;
 }
