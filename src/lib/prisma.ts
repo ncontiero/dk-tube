c2530731317a672bff92ebab8753cc01599f1386
dkshs <88355080+dkshs@users.noreply.github.com>
2025-03-16 00:34:37 -0300

    chore: update prisma client startup

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,7 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 import { env } from "@/env";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma =
-  globalForPrisma.prisma ??
+const createPrismaClient = () =>
   new PrismaClient({
     omit: {
       user: { externalId: true, email: true },
@@ -14,6 +9,12 @@ export const prisma =
       playlist: { userId: true },
     },
   });
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 export const prismaSkip = Prisma.skip;
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
